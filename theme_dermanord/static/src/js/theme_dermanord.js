e6ba3f12c111fc6408b6b36b3722b4293b959fbc
Haojun Zou <apollo_zhj@msn.com>
2016-11-16 10:30:22 +0100

    theme_dermanord: removed script

--- theme_dermanord/static/src/js/theme_dermanord.js
+++ theme_dermanord/static/src/js/theme_dermanord.js
@@ -1,10 +1,1 @@
-function show_me(){
-    // desktop size
-    if ($(".collapse").is(":visible"))
-        window.alert("desktop");
-    // mobile size
-    if (!$(".collapse").is(":visible"))
-        window.alert("mobile");
-}
-
 
