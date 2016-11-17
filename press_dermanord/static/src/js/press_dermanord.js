3b61c40c401682f67ec986aa76e50dec28827fa8
Haojun Zou <apollo_zhj@msn.com>
2016-11-17 21:34:07 +0100

    press_dermanord: temporary method for image links

--- press_dermanord/static/src/js/press_dermanord.js
+++ press_dermanord/static/src/js/press_dermanord.js
@@ -1,15 +1,21 @@
 // .css_editable_mode_hidden: view mode
 // .css_non_editable_mode_hidden: edit mode
-$('.img_press_block').each(function () {
-    var url = $(this).find('.css_editable_mode_hidden').attr('src');
-    var w = openerp.website.session.Model('website');
-    var res = w.call('get_id_from_url', [url], {});
+
+$(".img_press_block").each(function () {
+    var img_url = $(this).find(".img_2_change").attr("src");
+    var id = img_url.substring(img_url.lastIndexOf("ir.attachment/")+14, img_url.lastIndexOf("_"));
+    $(this).find(".img_press_website").attr("href", "/imagemagick/" + id + "/id/" + $(".website_recipe").attr("id"));
+    $(this).find(".img_press_original").attr("href", "/imagemagick/" + id + "/id/" + $(".original_recipe").attr("id"));
+
+
+    //~ var w = openerp.website.session.Model('website');
+    //~ var res = w.call('get_id_from_url', [url], {});
 
     //~ .then(function (result) {
         //~ window.alert('hello');
     //~ }, function(r){ window.alert('ah');}
     //~ );
-    console.log(res);
+    //~ console.log(res);
     //~ openerp.jsonRpc("/crm/todo/create", 'call', {
         //~ 'partner': partner,
         //~ 'memo': $("input[name=memo]").val(),
