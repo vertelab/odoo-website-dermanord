1b3c9f4de6cd82795af9207d6ebf68977441a7fd
Haojun Zou <apollo_zhj@msn.com>
2016-11-18 08:55:32 +0100

    press_dermanord: using one image

--- press_dermanord/static/src/js/press_dermanord.js
+++ press_dermanord/static/src/js/press_dermanord.js
@@ -4,6 +4,7 @@
 $(".img_press_block").each(function () {
     var img_url = $(this).find(".img_2_change").attr("src");
     var id = img_url.substring(img_url.lastIndexOf("ir.attachment/")+14, img_url.lastIndexOf("_"));
+    $(this).find("img .img_2_change").attr("src", "/imagemagick/" + id + "/id/" + $(".thumbnail_recipe").attr("id"));
     $(this).find(".img_press_website").attr("href", "/imagemagick/" + id + "/id/" + $(".website_recipe").attr("id"));
     $(this).find(".img_press_original").attr("href", "/imagemagick/" + id + "/id/" + $(".original_recipe").attr("id"));
 
