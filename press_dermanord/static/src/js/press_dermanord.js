80b7b6f3a09ce3a60d870821d6520887a709a01c
Haojun Zou <apollo_zhj@msn.com>
2016-11-18 16:49:52 +0100

    press_dermanord: update press template

--- press_dermanord/static/src/js/press_dermanord.js
+++ press_dermanord/static/src/js/press_dermanord.js
@@ -1,6 +1,3 @@
-// .css_editable_mode_hidden: view mode
-// .css_non_editable_mode_hidden: edit mode
-
 $(".img_press_block").each(function () {
     var img_url = $(this).find(".img_2_change").attr("src");
     if(img_url.indexOf("imagemagick") == -1) {
@@ -9,29 +6,6 @@ $(".img_press_block").each(function () {
         $(this).find(".img_press_original").attr("href", "/imagemagick/" + id + "/id/" + $(".original_recipe").attr("id"));
         $(this).find(".img_2_change").attr("src", "/imagemagick/" + id + "/id/" + $(".thumbnail_recipe").attr("id"));
     }
-
-
-    //~ var w = openerp.website.session.Model('website');
-    //~ var res = w.call('get_id_from_url', [url], {});
-
-    //~ .then(function (result) {
-        //~ window.alert('hello');
-    //~ }, function(r){ window.alert('ah');}
-    //~ );
-    //~ console.log(res);
-    //~ openerp.jsonRpc("/crm/todo/create", 'call', {
-        //~ 'partner': partner,
-        //~ 'memo': $("input[name=memo]").val(),
-    //~ }).done(function(data){
-        //~ if(data == "note_created"){
-            //~ $("#todo_table").load(document.URL + " #todo_table");
-            //~ $("#note_memo").val("");
-            //~ $("#note_button_group").addClass("hidden");
-            //~ $("#show_note_button_group").removeClass("hidden");
-        //~ }
-    //~ });
-
-
 });
 
 
