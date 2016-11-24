0acf528180d7a854e28ef79c3b598d15387c1fe8
Anders Wallenquist <anders.wallenquist@vertel.se>
2016-11-24 17:43:43 +0100

    webshop_dermanord: new dependencies

--- webshop_dermanord/__openerp__.py
+++ webshop_dermanord/__openerp__.py
@@ -30,7 +30,7 @@ Special layout for Dermanord AB webshop
 """,
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
-    'depends': ['website_sale', 'website_blog', 'website_sale_previous_products'],
+    'depends': ['website_sale', 'website_blog', 'website_sale_previous_products','website_product_pcategory','website_imagemagick'],
     'data': [
         'webshop_dermanord_view.xml',
         'webshop_dermanord_data.xml',
