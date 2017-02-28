5019cd5a321d805eb9774417c13089036ae8a708
Haojun Zou <apollo_zhj@msn.com>
2017-02-28 19:01:29 +0100

    theme_dermanord: class -> id for menu button and search button

--- theme_dermanord/static/src/js/theme_dermanord.js
+++ theme_dermanord/static/src/js/theme_dermanord.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $(".search-btn").on('click', function () {
+    $("#search-btn").on('click', function () {
         $("#desktop_logo").toggle();
         $("#mobile_logo").toggle();
         $("#search_input").toggle();
