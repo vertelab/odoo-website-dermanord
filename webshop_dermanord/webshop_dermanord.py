c040152927ef9e6003f8183f0590d7a153c23c4b
Haojun Zou <apollo_zhj@msn.com>
2017-03-20 18:32:30 +0100

    webshop_dermanord: default variant

--- webshop_dermanord/webshop_dermanord.py
+++ webshop_dermanord/webshop_dermanord.py
@@ -52,6 +52,12 @@ class product_template(models.Model):
     blog_post_ids = fields.Many2many(comodel_name='blog.post', string='Posts', compute='_blog_post_ids')
 
 
+class product_product(models.Model):
+    _inherit = 'product.product'
+
+    default_variant = fields.Boolean(string='Default Variant')
+
+
 class website_sale(website_sale):
 
     @http.route([
@@ -127,3 +133,9 @@ class website_sale(website_sale):
             'attrib_encode': lambda attribs: werkzeug.url_encode([('attrib',i) for i in attribs]),
         }
         return request.website.render("webshop_dermanord.products", values)
+
+    @http.route([
+        '/shop/<model("product.template"):template>/variant/<model("product.product"):product>'
+    ], type='http', auth="public", website=True)
+    def dn_product_variant(self, template=None, product=None, **post):
+        return request.website.render('website_sale.product', {'product': template, 'product_product': product})
