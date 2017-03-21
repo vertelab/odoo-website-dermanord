75112b40eba13c01b2142ed4eaf741b899a91796
Haojun Zou <apollo_zhj@msn.com>
2017-03-21 12:16:24 +0100

    webshop_dermanord, snippet_dermanord: updated depends

--- snippet_dermanord/__openerp__.py
+++ snippet_dermanord/__openerp__.py
@@ -31,7 +31,6 @@ Extra snippets for Dermanord.
     'author': 'Vertel AB',
     'website': 'http://www.vertel.se',
     'depends': [
-        'website',
         'webshop_dermanord',
         'website_imagemagick',
         'sale_promotions',
