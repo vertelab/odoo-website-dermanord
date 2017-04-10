e0fd2614ebeb705cdad0c8ef1cb7ab3cbf7feac3
Haojun Zou <apollo_zhj@msn.com>
2017-04-10 22:32:58 +0200

    webshop_dermanord: updated css

--- webshop_dermanord/static/src/js/webshop_search.js
+++ webshop_dermanord/static/src/js/webshop_search.js
@@ -1,10 +1,12 @@
+var times = 0;
+
 $(document).ready(function(){
 
     $(".fts_result").find("h3, h4, h5, span").each(function(){
         console.log($(this).text());
         if($(this).text().toLowerCase().indexOf(dermanord_kw) > -1){
-            content = $(this).text();
-            content_arr = content.split(dermanord_kw); //TODO: case insensitve on regex
+            var content = $(this).text();
+            var content_arr = content.split(dermanord_kw); //TODO: case insensitve on regex
             $(this).html(content_arr[0] + "<span class='dn_kw'>" + dermanord_kw + "</span>" + content_arr[1]);
         }
     });
@@ -19,5 +21,7 @@ $(window).scroll(function() {
     if($(window).scrollTop() == $(document).height() - $(window).height()) {
            // ajax call get data from server and append to the div
         console.log('bottom');
+        $(".question").append();
+        times ++;
     }
 });
