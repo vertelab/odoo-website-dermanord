05ea97a453331b7dc5b23591ea0b8db57a17f97c
Haojun Zou <apollo_zhj@msn.com>
2017-04-10 17:26:16 +0200

    webshop_dermanord: updated search result template

--- webshop_dermanord/static/src/js/webshop_search.js
+++ webshop_dermanord/static/src/js/webshop_search.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 
+    $(".fts_result").find("h3, h4, h5, span").each(function(){
+        console.log($(this).text());
+        if($(this).text().toLowerCase().indexOf(dermanord_kw) > -1){
+            content = $(this).text();
+            content_arr = content.split(dermanord_kw); //TODO: case insensitve on regex
+            $(this).html(content_arr[0] + "<span class='dn_kw'>" + dermanord_kw + "</span>" + content_arr[1]);
+        }
+    });
+
     $(document).ready(function(){
         $(".oe_select9_dn_search").select9();
     });
 
+});
+
+$(window).scroll(function() {
+    if($(window).scrollTop() == $(document).height() - $(window).height()) {
+           // ajax call get data from server and append to the div
+        console.log('bottom');
+    }
 });
