a0372dc060e669a4462a7bd1981a15d645d15880
Haojun Zou <apollo_zhj@msn.com>
2017-05-16 16:13:30 +0200

    theme_dermanord: add shopping cart

--- webshop_dermanord/__openerp__.py
+++ webshop_dermanord/__openerp__.py
@@ -32,7 +32,7 @@ Special layout for Dermanord AB webshop
     'website': 'http://www.vertel.se',
     'depends': ['product_multi_image', 'website_sale_product_gallery', 'website_blog', 'product_dermanord',
             'website_sale_previous_products','website_product_pcategory','website_imagemagick', 'product_ingredients',
-            'product_facets', 'website_bootstrap_select2', 'website_fts_product', 'product_attribute_value_image'
+            'product_facets', 'website_bootstrap_select2', 'website_fts_product', 'product_attribute_value_image',
         ],
     'data': [
         'webshop_dermanord_view.xml',
