f5df38b21adebc47f39f607fcbb14b1c3ca370a4
Haojun Zou <apollo_zhj@msn.com>
2017-06-20 11:45:45 +0200

    theme_dermanord: defined submenu inside template

--- theme_dermanord/static/src/js/theme_dermanord.js
+++ theme_dermanord/static/src/js/theme_dermanord.js
@@ -2,14 +2,13 @@ var search_icron = $("#search-btn-modal").closest("li");
 var shop_cart = $("a[href='/shop/cart']").closest("li");
 var divider = $(".divider").closest("li");
 var my_account = $(".dropdown-menu.js_usermenu").closest("li");
-var footer_menu = $("li.footer_menu");
 var more_menu = $("li#more_dropdown");
 var menu_items = [];
 var more_menu_items = [];
 $("#top_menu").children().each(function() {
-    if($(this).attr("class") == "footer_menu dropdown hidden-lg hidden-md hidden-sm ")
+    if($(this).attr("id") == "fts_search_btn")
         return false;
-    else
+    else if($(this).attr("id") != "more_dropdown")
         menu_items.push($(this));
 });
 $("#more_dropdown").find("ul").children().each(function() {
@@ -18,9 +17,10 @@ $("#more_dropdown").find("ul").children().each(function() {
 
 var li_width_init = search_icron.width() + shop_cart.width() + divider.width() + my_account.width() + 40; // extra pixels to avoid change row
 if(menu_items.length == 0) {
-    more_menu.css({"display": "none"});
+    more_menu.addClass("hidden");
 }
 else if(menu_items.length != 0) {
+    more_menu.removeClass("hidden");
     li_width_init += more_menu.width();
 }
 
