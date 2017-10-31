f111612a37a97d5211b0adecd9229678f2471fd2
Anders Wallenquist <anders.wallenquist@vertel.se>
2017-10-31 11:32:47 +0100

    webshop_dermanord: disable add_to_cart

--- webshop_dermanord/static/src/js/webshop_dermanord.js
+++ webshop_dermanord/static/src/js/webshop_dermanord.js
@@ -48,6 +48,24 @@ $(document).ready(function(){
             $(this).closest(".checkbox").find(".like").removeAttr('checked');
         }
     });
+    var $add_to_cart = $("#add_to_cart");
+    $add_to_cart.find('a').attr('disabled','disabled');
+    
+    //~ $("#terms_and_conditions").find("[name='terms_and_conditions']").on('change', function () {
+        //~ openerp.jsonRpc("/shop/order/terms_and_conditions", 'call', {
+            //~ 'accepted': $(this).val(),
+        //~ });
+    //~ });
+    //~ $("#terms_and_conditions").on('click', function(ev){
+        //~ if ($('#terms_and_conditions:checked').length != 0) {
+            //~ $payment.find('button').removeAttr('disabled');
+        //~ }
+        //~ else {
+            //~ $payment.find('button').attr('disabled','disabled');
+        //~ }
+    //~ });
+    
+
 
     function update_product_image(event_source, product_id) {
         var $img = $(event_source).closest('tr.js_product, .oe_website_sale').find('span[data-oe-model^="product."][data-oe-type="image"] img:first, img.product_detail_img');
