81e92a0decc39e9cdf80ee2d2fe8544730125001
Haojun <apollo_zhj@msn.com>
2018-02-15 10:52:28 +0100

    webshop_memcached_dermanord: parameter changed

--- webshop_memcached_dermanord/website.py
+++ webshop_memcached_dermanord/website.py
@@ -72,7 +72,7 @@ class WebsiteSale(WebsiteSale):
 
     # '/dn_shop/product/<model("product.template"):product>'
     @memcached.route(key=lambda kw:'db: {db} path: {path} logged_in: {logged_in} lang: {lang}%s' % request.website.get_search_values(kw), flush_type='dn_shop')
-    def product(self, product, category='', search='', **kwargs):
+    def product(self, product, category='', search='', **post):
         return super(WebsiteSale, self).product(product, category, search, **post)
 
     #~ @http.route([
