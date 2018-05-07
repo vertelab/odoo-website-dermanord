a3ce1145c6c59cc5b500fdc40acada6b0849ee40
Robin Chatfield <robin.chatfield@vertel.se>
2018-05-07 15:33:50 +0200

    website_cookie_notice_dermanord: Changed to handle cookie notification through javascript.

--- website_cookie_notice_dermanord/static/src/js/main.js
+++ website_cookie_notice_dermanord/static/src/js/main.js
@@ -1,2 +1,10 @@
 $(document).ready(function() {
+    if (Cookies.get("cookie_notification_accept") !== "1") {
+        $("#website_cookie_notice").show();
+    };
 });
+
+function cookie_notification_accept() {
+        Cookies.set("cookie_notification_accept",  "1");
+        $("#website_cookie_notice").hide();
+    }
