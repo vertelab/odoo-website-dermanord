8e1ca79a306ebb3bff43b7446d0a3cc6da3a2f7b
Haojun <apollo_zhj@msn.com>
2018-05-15 11:28:19 +0200

    website_blog_dermanord: resize height of blog background image

--- website_blog_dermanord/static/src/js/main.js
+++ website_blog_dermanord/static/src/js/main.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
-
+    resize_blog_bg_image();
 });
+
+$(window).resize(function() {
+    resize_blog_bg_image();
+});
+
+function resize_blog_bg_image() {
+    if($(window).width() < 992) {
+        $(".blog_background_image_list").css({"height": $(".blog_background_image_list").width() / 3.035555556});
+    }
+}
