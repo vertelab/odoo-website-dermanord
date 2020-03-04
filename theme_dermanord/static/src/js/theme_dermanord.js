2a34c4fb79b94beed1465e9e4a74fa6ae8ec3d77
Daniel Eriksson <daniel.eriksson@vertel.se>
2020-03-04 14:12:33 +0100

    theme_dermanord: bugfix

--- theme_dermanord/static/src/js/theme_dermanord.js
+++ theme_dermanord/static/src/js/theme_dermanord.js
@@ -37,24 +37,15 @@ dermanord_resize_for_menu = function() {
         var li_width = 0;
         $.each(menu_items, function(index, value) {
 
-            // console.log(value.attr('id'))
-
-            // TODO: this code is not working. at all.
-            // Several links has href="#" and links are not the same in top menu and more menu
-            // TODO: add id to li-tags for menu options. Add id to a-tags in more-menu. Rewrite search 
-            // more_menu_item = more_menu.find('a[href="' + value.find('a').attr('href') + '"]');
             more_menu_item = more_menu.find('li#' + value.attr('id') + '-more');
 
             if (more_menu_item.length == 1) {
-                // li_width += $(this).width();
                 li_width += value.width();
                 if (li_width > max_li_width) { // hide this menu item from menu bar and show it in more menu
-                    // $(this).closest("li").css({"display": "none"});
                     value.closest("li").css({"display": "none"});
                     more_menu_item.css({"display": "block"});
                 }
                 else {  // show this menu item in menu bar again and hide it from more menu
-                    // $(this).closest("li").css({"display": "block"});
                     value.closest("li").css({"display": "block"});
                     more_menu_item.css({"display": "none"});
                 }
