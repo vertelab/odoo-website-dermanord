10f02de364bcc42372910828525bb2272f05df1c
victor ekstrom <victor.ekstrom@vertel.se>
2021-04-09 14:28:48 +0000

    res_users_link: gjort nils glad(rengjort koden)

--- res_users_link/__manifest__.py
+++ res_users_link/__manifest__.py
@@ -7,7 +7,8 @@
     """,
 
     'description': """
-        links from user to partner
+        links from user to partner \n
+        v14.0.0.0.1: initial version
     """,
 
     'author': "Vertel AB",
@@ -17,7 +18,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
-    'version': '0.001',
+    'version': '14.0.0.0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
