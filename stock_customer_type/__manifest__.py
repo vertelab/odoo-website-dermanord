f96969ae7d81ed1d2560cc2c2946f4bbd7b09949
Victor Ekström <eksvic@barney.vertel.se>
2021-04-12 09:32:28 +0000

    stock_picking_customer: fixed a bug and added the customer_type to the printed out picking orders aswell

--- stock_customer_type/__manifest__.py
+++ stock_customer_type/__manifest__.py
@@ -26,7 +26,6 @@
     'data': [
         # 'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
