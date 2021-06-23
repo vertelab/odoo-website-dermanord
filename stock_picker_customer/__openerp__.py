109c21038cc3f77cded1ac35593f69eaa560a148
Daniel Löfgren <daniel.lofgren@vertel.se>
2021-06-23 10:49:57 +0200

    stock_picker_customer 20711: Refactoring and translations

--- stock_picker_customer/__openerp__.py
+++ stock_picker_customer/__openerp__.py
@@ -25,8 +25,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views.xml',
-        'template.xml'
+        'views/views.xml',
+        'views/template.xml'
     ],
 }
