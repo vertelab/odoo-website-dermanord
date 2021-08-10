feb8ddd8029166d5c496993ac1b1969e70690450
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-08-10 14:44:22 +0000

    theme_maria_akerberg: removed invoices and quotations from my page in odoo portal

--- theme_maria_akerberg/__manifest__.py
+++ theme_maria_akerberg/__manifest__.py
@@ -1,5 +1,4 @@
 {
-    # Theme information
     'name': "Maria Åkerberg",
     'description': """
     """,
@@ -7,27 +6,20 @@
     'version': '1.0',
     'depends': ['website', 'website_sale', 'website_imagemagick',
     ],
-
-    # templates
     'data': [
-        # ~ 'views/options.xml',
-        # ~ 'views/snippets.xml',
         'views/assets.xml',
         'views/variant_template.xml',
         'views/templates.xml',
         'views/recipe.xml',
         'views/customizations.xml',
         'views/product_views.xml',
+        'views/portal_templates.xml',
     ],
-
-    # demo pages
     'demo': [
-        # ~ 'demo/pages.xml',
     ],
     # misc settings
     'installable': True,
     'application': False,
-
     # Your information
     'author': "Vertel AB",
     'website': "www.vertel.se",
