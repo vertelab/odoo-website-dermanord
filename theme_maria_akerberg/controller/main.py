0588496f2d89b1e3a5dd86763df3da79722d0be6
Miracle Ayodele <miracleaayodele@gmail.com>
2021-08-11 14:05:21 +0100

    Default A-Z Sorting

---
+++ theme_maria_akerberg/controller/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import logging
+
+from odoo import fields, http, SUPERUSER_ID, tools, _
+from odoo.addons.website_sale.controllers.main import WebsiteSale
+_logger = logging.getLogger(__name__)
+
+
+class WebsiteSaleImp(WebsiteSale):
+    def _get_search_order(self, post):
+        print("Hello")
+        # OrderBy will be parsed in orm and so no direct sql injection
+        # id is added to be sure that order is a unique sort key
+        order = post.get('order') or 'name ASC'
+        return 'is_published desc, %s, id desc' % order
