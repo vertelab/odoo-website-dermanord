7f72d9909bdfa78d7fb569b05730147451be6db7
Daniel Löfgren <daniel.lofgren@vertel.se>
2021-08-25 05:46:08 +0000

    currency_decimal_options module added, for granular price printing

---
+++ currency_decimal_options/models/ir_qweb_fields.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _, _lt
+
+from odoo.tools import html_escape as escape, posix_to_ldml, float_utils, format_date, format_duration, pycompat
+from odoo.tools.misc import get_lang, babel_locale_parse
+
+import logging
+_logger = logging.getLogger(__name__)
+
+class MonetaryConverter(models.AbstractModel):
+    _inherit = 'ir.qweb.field.monetary'
+
+    @api.model
+    def value_to_html(self, value, options):
+        display_currency = options['display_currency']
+
+        if not isinstance(value, (int, float)):
+            raise ValueError(_("The value send to monetary field is not a number."))
+
+        # lang.format mandates a sprintf-style format. These formats are non-
+        # minimal (they have a default fixed precision instead), and
+        # lang.format will not set one by default. currency.round will not
+        # provide one either. So we need to generate a precision value
+        # (integer > 0) from the currency's rounding (a float generally < 1.0).
+        decimal_places = options.get('decimal_places', display_currency.decimal_places)
+        fmt = "%.{0}f".format(decimal_places)
+
+        if options.get('from_currency'):
+            date = options.get('date') or fields.Date.today()
+            company_id = options.get('company_id')
+            if company_id:
+                company = self.env['res.company'].browse(company_id)
+            else:
+                company = self.env.company
+            value = options['from_currency']._convert(value, display_currency, company, date)
+
+        lang = self.user_lang()
+        formatted_amount = lang.format(fmt, display_currency.round(value),
+                                grouping=True, monetary=True).replace(r' ', u'\N{NO-BREAK SPACE}').replace(r'-', u'-\N{ZERO WIDTH NO-BREAK SPACE}')
+
+        pre = post = u''
+        if display_currency.position == 'before':
+            pre = u'{symbol}\N{NO-BREAK SPACE}'.format(symbol=display_currency.symbol or '')
+        else:
+            post = u'\N{NO-BREAK SPACE}{symbol}'.format(symbol=display_currency.symbol or '')
+
+        return u'{pre}<span class="oe_currency_value">{0}</span>{post}'.format(formatted_amount, pre=pre, post=post)
