27a6ea9cdb0b95ba36fbf2251554aba7986e9da1
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-08-26 12:13:35 +0000

    theme_clarico_maria_akerberg: initial commit

---
+++ theme_clarico_maria_akerberg/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    "name": "Maria Åkerberg (clarico)",
+    "description": """
+Website theme for Maria Åkerberg, based on Clarico
+    """,
+    "category": "Theme/Vertel",
+    "version": "1.0",
+    "depends": [
+        "theme_clarico",
+    ],
+    "data": [
+        "security/base_security.xml",
+    ],
+    "demo": [],
+    "installable": True,
+    "application": False,
+    "author": "Vertel AB",
+    "website": "www.vertel.se",
+}
