3b902e6fc7b5a5a6dd9e67da427ae60c065f4e89
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-09-03 14:32:20 +0000

    product_ma_fields, webshop_from_price: pep8

--- product_ma_fields/models/product_template.py
+++ product_ma_fields/models/product_template.py
@@ -9,16 +9,30 @@ _logger = logging.getLogger(__name__)
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    def _get_combination_info(self, combination=False, product_id=False, add_qty=1, pricelist=False, parent_combination=False, only_template=False):
-        res = super(ProductTemplate, self)._get_combination_info(combination=combination, product_id=product_id, add_qty=add_qty, pricelist=pricelist, parent_combination=parent_combination, only_template=only_template)
+    def _get_combination_info(
+        self,
+        combination=False,
+        product_id=False,
+        add_qty=1,
+        pricelist=False,
+        parent_combination=False,
+        only_template=False,
+    ):
+        res = super(ProductTemplate, self)._get_combination_info(
+            combination=combination,
+            product_id=product_id,
+            add_qty=add_qty,
+            pricelist=pricelist,
+            parent_combination=parent_combination,
+            only_template=only_template,
+        )
 
-        product = self.env['product.product'].browse(res['product_id'])
+        product = self.env["product.product"].browse(res["product_id"])
 
-        res['description_webshop'] = product.description_webshop
-        res['description_use'] = product.description_use
-        res['description_ingredients'] = product.description_ingredients
+        res["description_webshop"] = product.description_webshop
+        res["description_use"] = product.description_use
+        res["description_ingredients"] = product.description_ingredients
 
-        _logger.warning(f'RES is: {res}')
         return res
