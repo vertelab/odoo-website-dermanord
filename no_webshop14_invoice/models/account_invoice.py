96b231ea5191aa718bd03637e225ae768cf356e4
Daniel Eriksson <daniel.eriksson@vertel.se>
2021-09-28 14:22:56 +0200

    no_webshop14_invoice: bugfix

--- no_webshop14_invoice/models/account_invoice.py
+++ no_webshop14_invoice/models/account_invoice.py
@@ -13,9 +13,8 @@ class mail_compose_message(models.Model):
         context = self._context
         if context.get('default_model') == 'account.invoice' and \
                 context.get('default_res_id') and context.get('mark_invoice_as_sent'):
-            # Hopefully no customer has WSSO in their name...
             invoice = self.env['account.invoice'].browse(context['default_res_id'])
-            if invoice.invoice_type_id == self.env.ref("invoice_type.invoice_webshop"):
+            if invoice.order_id and invoice.order_id.invoice_type_id == self.env.ref("invoice_type.invoice_webshop"):
                 return
             invoice = invoice.with_context(mail_post_autofollow=True)
             invoice.write({'sent': True})
