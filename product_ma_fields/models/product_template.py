1c6a451e7799d8d2c3e72e7dc2603f3d4ea8a14b
Daniel Löfgren <dan.lofgrens@gmail.com>
2021-10-26 06:24:05 +0000

    [215355] | Update produt template controller with label data

--- product_ma_fields/models/product_template.py
+++ product_ma_fields/models/product_template.py
@@ -32,7 +32,8 @@ class ProductTemplate(models.Model):
         product = self.env["product.product"].browse(res["product_id"])
         template = self.env["product.template"].browse(res["product_template_id"])
 
-        product_variant_lables = product.mapped('label_line_variant_ids').mapped('label').mapped('display_name')
+        product_variant_labels = product.mapped('label_line_variant_ids').mapped('label').mapped('display_name')
+        product_template_labels = product.mapped('label_line_ids').mapped('label').mapped('display_name')
 
         # Check if there is potential for more than one product combination
         has_variants = False
@@ -40,7 +41,8 @@ class ProductTemplate(models.Model):
             if len(attribute_line.value_ids) > 1:
                 has_variants = True
 
-        res['product_variant_labels'] = product_variant_lables
+        res['product_labels'] = product_template_labels
+        res['product_variant_labels'] = product_variant_labels
         res["description_webshop"] = product.description_webshop
         res["description_use"] = product.description_use
         res["description_ingredients"] = product.description_ingredients
