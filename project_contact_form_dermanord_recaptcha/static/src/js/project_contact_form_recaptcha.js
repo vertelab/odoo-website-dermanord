abfb57199717869f0c3543c915e00f2680feee96
Simon Rundstedt <simon.rundstedt@vertel.se>
2021-11-04 15:48:01 +0100

    [21595] Overload JS to toggle Send-button

--- project_contact_form_dermanord_recaptcha/static/src/js/project_contact_form_recaptcha.js
+++ project_contact_form_dermanord_recaptcha/static/src/js/project_contact_form_recaptcha.js
@@ -4,8 +4,38 @@ function enable_send_button (){
 }
 function disable_send_button (){
     $("button[class='btn btn-primary btn-lg']").prop("disabled",true)
-    console.log("Send button enabled")
+    console.log("Send button disabled")
 }
+
+/**
+ * Shamelessly taken from website_form_recaptcha/src/js/field_recaptcha.js
+ * WARNING: This makes it hard to extend the functionality of the reCAPTCHA
+ * module.
+ */
+(function ($) {
+  "use strict";
+  openerp.website.snippet.animationRegistry.form_recaptcha
+  = openerp.website.snippet.Animation.extend({
+    selector: ".o_website_form_recaptcha",
+    start: function() {
+      var self = this;
+      this._super();
+      $.ajax({
+        url: '/website/recaptcha/',
+        method: 'GET',
+        success: function(data){
+          data = JSON.parse(data);
+          self.$target.append($(
+            '<div class="g-recaptcha" data-sitekey="' + data.site_key + '" data-callback="enable_send_button" data-expired-callback="disable_send_button"></div>'
+          ));
+          $.getScript('https://www.google.com/recaptcha/api.js');
+        },
+      });
+    }
+
+  });
+
+})(jQuery);
 
 // Disable and set enable as reCAPTCHA callback: data-callback
 disable_send_button()
