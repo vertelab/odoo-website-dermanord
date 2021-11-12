8124ef5a7729910a5a2f63c16ab8497a55c96496
Simon Rundstedt <simon.rundstedt@vertel.se>
2021-11-12 12:22:30 +0100

    [21838] Change search query for form button

--- project_contact_form_dermanord_recaptcha/static/src/js/project_contact_form_recaptcha.js
+++ project_contact_form_dermanord_recaptcha/static/src/js/project_contact_form_recaptcha.js
@@ -1,9 +1,9 @@
 function enable_send_button (){
-    $("button[class='btn btn-primary btn-lg']").prop("disabled",false)
+    $("button[id='contact-form-btn'][class='btn btn-primary btn-lg']").prop("disabled",false)
     console.log("Send button enabled")
 }
 function disable_send_button (){
-    $("button[class='btn btn-primary btn-lg']").prop("disabled",true)
+    $("button[id='contact-form-btn'][class='btn btn-primary btn-lg']").prop("disabled",true)
     console.log("Send button disabled")
 }
 
