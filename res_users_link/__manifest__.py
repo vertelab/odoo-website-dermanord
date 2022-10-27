5f68531205d27a2efcc0f04205795766efc3d47c
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-27 11:49:25 +0200

    Update __manifest__.py

--- res_users_link/__manifest__.py
+++ res_users_link/__manifest__.py
@@ -1,26 +1,45 @@
 # -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 {
-    'name': "res_users_link",
-
-    'summary': """
-        links from user to partner
-    """,
-
-    'description': """
-        links from user to partner \n
-        v14.0.0.0.1: initial version
-    """,
-
-    'author': "Vertel AB",
-    'website': "http://www.yourcompany.com",
-
+    'name': 'Dermanord: Res Users Lnk',
+    'version': '14.0.0.0.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Links from partner.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '14.0.0.0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Website',
+    'description': """
+    Links from partner.
+    """,
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website-dermanord/res_users_link',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    'repository': 'https://github.com/vertelab/odoo-website-dermanord',
+    # Any module necessary for this one to work correctly
     'depends': ['base'],
 
     # always loaded
@@ -31,3 +50,4 @@
     'demo': [
     ],
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
