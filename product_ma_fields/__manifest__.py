7e5a804834dff076442b4f30aab6f34c73dc585b
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-27 11:31:04 +0200

    Update __manifest__.py

--- product_ma_fields/__manifest__.py
+++ product_ma_fields/__manifest__.py
@@ -1,7 +1,8 @@
-################################################################################
+# -*- coding: utf-8 -*-
+##############################################################################
 #
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2019 N-Development (<https://n-development.com>).
+#    Odoo SA, Open Source Management Solution, third party addon
+#    Copyright (C) 2022- Vertel AB (<https://vertel.se>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -14,17 +15,32 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-################################################################################
+##############################################################################
 
 {
-    'name': 'MA webshop extra product fields',
+    'name': 'Dermanord: MA webshop extra product fields',
+    'version': '14.0.1.2.0',
+    # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
+    'summary': 'Adds additional product fields.',
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Website',
     'description': """
     Adds additional product fields.
     """,
-    'category': 'Util/Vertel',
-    'version': '14.0.1.2.0',
+    #'sequence': '1',
+    'author': 'Vertel AB',
+    'website': 'https://vertel.se/apps/odoo-website-dermanord/product_ma_fields',
+    'images': ['static/description/banner.png'], # 560x280 px.
+    'license': 'AGPL-3',
+    'contributor': '',
+    'maintainer': 'Vertel AB',
+    # Any module necessary for this one to work correctly
+    'repository': 'https://github.com/vertelab/odoo-website-dermanord',
+
     'depends': [
         "sale",
         "website_sale",
@@ -38,6 +54,5 @@
     ],
     'installable': True,
     'application': False,
-    'author': "Vertel AB",
-    'website': "www.vertel.se",
 }
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
