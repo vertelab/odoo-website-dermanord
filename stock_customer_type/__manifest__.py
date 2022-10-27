eb3b16e27b6c283074e40782502dad2b0c438857
jakobkrabbe <33831118+jakobkrabbe@users.noreply.github.com>
2022-10-27 13:34:32 +0200

    Update __manifest__.py

--- stock_customer_type/__manifest__.py
+++ stock_customer_type/__manifest__.py
@@ -20,17 +20,16 @@
 ##############################################################################
 
 {
-    'name': 'Dermanord: Stock CustomerType',
+    'name': 'Dermanord: Stock Customer Type',
     'version': '14.0.0.0.0',
     # Version ledger: 14.0 = Odoo version. 1 = Major. Non regressionable code. 2 = Minor. New features that are regressionable. 3 = Bug fixes
-    'summary': 'Short (1 phrase/line) summary of the module''s purpose',
+    'summary': 'Summary to come.',
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Website',
     'description': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com    
+        Description to come.   
     """,
     #'sequence': '1',
     'author': 'Vertel AB',
